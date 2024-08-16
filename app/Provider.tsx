4dd9f92755fae2a996f04b59d16eaa88a656b98b
Danilo <danilodecanini@gmail.com>
2024-08-15 22:19:35 -0300

    feat: add active collaborators component live

--- app/Provider.tsx
+++ app/Provider.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import Loader from '@/components/Loader'
+import { getClerkUser } from '@/lib/actions/user.actions'
 import {
   ClientSideSuspense,
   LiveblocksProvider
@@ -9,7 +10,14 @@ import React, { type ReactNode } from 'react'
 
 const Provider = ({ children }: { children: ReactNode }) => {
   return (
-    <LiveblocksProvider authEndpoint='/api/liveblocks-auth'>
+    <LiveblocksProvider
+      resolveUsers={async ({ userIds }) => {
+        const users = await getClerkUser({ userIds })
+
+        return users
+      }}
+      authEndpoint='/api/liveblocks-auth'
+    >
       <ClientSideSuspense fallback={<Loader />}>{children}</ClientSideSuspense>
     </LiveblocksProvider>
   )
--- lib/utils.ts
+++ lib/utils.ts
@@ -5,8 +5,8 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
 
-// biome-ignore lint/suspicious/noExplicitAny: <explanation>
-export const parseStringify = (value: any) => JSON.parse(JSON.stringify(value))
+export const parseStringify = (value: unknown) =>
+  JSON.parse(JSON.stringify(value))
 
 export const getAccessType = (userType: UserType) => {
   switch (userType) {
