70c040ca8cc4789f86c1378ec57f99fdfcdaf786
Danilo <danilodecanini@gmail.com>
2024-08-15 21:48:21 -0300

    feat: add document btn

--- app/(root)/[documents]/[id]/page.tsx
+++ app/(root)/[documents]/[id]/page.tsx
@@ -3,9 +3,9 @@ import React from 'react'
 
 const Document = () => {
   return (
-    <div>
+    <main className='flex w-full flex-col items-center'>
       <CollaborativeRoom />
-    </div>
+    </main>
   )
 }
 
--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,5 +1,47 @@
+import AddDocumentBtn from '@/components/AddDocumentBtn'
+import Header from '@/components/Header'
+import { SignedIn, UserButton } from '@clerk/nextjs'
+import { currentUser } from '@clerk/nextjs/server'
 import Image from 'next/image'
+import { redirect } from 'next/navigation'
 
-export default function Home() {
-  return <h1>Hello world</h1>
+const Home = async () => {
+  const clerkUser = await currentUser()
+  if (!clerkUser) redirect('/sign-in')
+
+  const documents = []
+
+  return (
+    <main className='home-container'>
+      <Header className='sticky left-o top-0'>
+        <div className='flex items-center gap-2 lg:gap-4'>
+          Notification
+          <SignedIn>
+            <UserButton />
+          </SignedIn>
+        </div>
+      </Header>
+
+      {documents.length > 0 ? (
+        <div>document here</div>
+      ) : (
+        <div className='document-list-empty'>
+          <Image
+            src='/assets/icons/doc.svg'
+            alt='Document'
+            width={40}
+            height={40}
+            className='mx-auto'
+          />
+
+          <AddDocumentBtn
+            userId={clerkUser.id}
+            email={clerkUser.emailAddresses[0].emailAddress}
+          />
+        </div>
+      )}
+    </main>
+  )
 }
+
+export default Home
